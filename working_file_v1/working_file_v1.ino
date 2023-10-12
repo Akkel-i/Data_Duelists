c4906ff9fdc2c47273a9ee03ffb14d5d2469e7f6
Akkel-i <118986817+Akkel-i@users.noreply.github.com>
2023-10-12 09:38:33 +0300

    updating
    UID tekee nyt numerot joiden avulla voi tehä statis ukkeleille

--- working_file_v1/working_file_v1.ino
+++ working_file_v1/working_file_v1.ino
@@ -14,7 +14,10 @@
 String Scanned_value_NFC;
 String NFC1;
 String NFC2;
-String muutettu_arvo;
+String convertedNFC1;
+String convertedNFC2;
+int arrayNFC1[4];
+int arrayNFC2[4];
 bool playersCreated = false;
 
 MFRC522 rfid(SS_PIN, RST_PIN);
@@ -36,12 +39,14 @@ void setup() {
 class CreatePlayer {
 public:
   // Constructor to initialize the variables
-  CreatePlayer(String nameValue, int strValue, int defValue, int agiValue, int conValue) {
+  CreatePlayer(String nameValue, int strValue, int defValue, int agiValue, int conValue, int winValue, int lossValue) {
     name = nameValue;
     str = strValue;
     def = defValue;
     agi = agiValue;
     con = conValue;
+    wins = winValue;
+    losses = lossValue;
   }
     String getName() {
     return name;
@@ -58,12 +63,26 @@ public:
   int getCon() {
     return con;
   }
+  int getWins() {
+    return wins;
+  }
+  int getLosses() {
+    return losses;
+  }
+    void addWins() {
+    wins++;
+  }
+  void addLosses() {
+    losses++;
+  }
 private:
   String name;
   int str;
   int def;
   int agi;
   int con;
+  int wins;
+  int losses;
 };
 
 
@@ -92,31 +111,51 @@ void loop() {
   Serial.println();
 
   // testiä vaa toimiiks toi UID arvon muutos numeroihin
-    if (!NFC1.length() == 0) {
+    if (NFC1.length() != 0 && NFC2.length() != 0) {
       Serial.println("funktio mikä muuttaa UID meidän arvoiksi: ");
-      for (int i = 0; i < NFC1.length(); i++) {
-        Serial.print(" " + UID_value(String(NFC1[i])));
-      }
-      Serial.println();
+        convertedNFC1 = convertUID(NFC1);
+        convertedNFC2 = convertUID(NFC2);
+        Serial.println(convertedNFC1 + " " + convertedNFC2);
+        Serial.println();
     }
 
-  /* testiä toimiiko muistiin tallentaminen ja lukeminen
+  /* testiä toimiiko muistiin tallentaminen ja lukeminen (pitäis toimii, ainakin simuloiden)
    *  if (NFC1.length() != 0) {
    * saveToMemory(NFC1);
    * }
-   * Serial.print("Muistista luettu arvo on: ");
-   * Serial.print(getFromMemory());
+   * Serial.println("Muistista luettu arvo on: ");
+   * Serial.println(getFromMemory());
    */
 
-  if ( playersCreated == false) {
-  // str, def, agi, con. Tällä kutsulla tekee 
-  CreatePlayer player1("Jonas", 10, 42, 10, 5);
-  CreatePlayer player2("Aku", 10, 42, 10, 5);
-  playersCreated = true;
+  //tekee UID numero-arvoista array(4) jossa statsit 0-9 arvoilla
+    createStats(converted1, arrayNFC1);
+    createStats(converted2, arrayNFC2);
+
+/*     näillä voi käydä läpi arrayn sisällöt
+    for (int i = 0; i < 4; i++) {
+    Serial.print("Result[" + String(i) + "]: ");
+    Serial.println(arrayNFC1[i]);
+    }
+    for (int i = 0; i < 4; i++) {
+        Serial.print("Result[" + String(i) + "]: ");
+        Serial.println(arrayNFC2[i]);
+    } */
+
+
+
+
+
+
+  // str, def, agi, con, voitot, häviöt. Tällä kutsulla tekee  (array kutsut pitäisi toimia ihan oikein, simulaatiossa)
+  CreatePlayer player1("Jonas", 10, 42, 10, 5, 0, 0 );
+  //CreatePlayer player1("Jonas", arrayNFC1[0], arrayNFC1[1], arrayNFC1[2], arrayNFC1[3], 0, 0 );
+  CreatePlayer player2("Aku", 10, 42, 10, 5, 0, 0 );
+  //CreatePlayer player2("Aku", arrayNFC2[0], arrayNFC2[1], arrayNFC2[2], arrayNFC2[3], 0, 0 );
+
     // statseja haetaan komennolla: player1.getDef()
     Serial.println("hahmot luotu ja ");
-    Serial.print("player1 deffa on: " +  String(player1.getDef()) + " ja player2 constitution on: " +  String(player2.getCon()));
-  }
+    Serial.println("player1 deffa on: " +  String(player1.getDef()) + " ja player2 constitution on: " +  String(player2.getCon()) + " Pelaajan1 nimi on: " + player1.getName());
+  
   
   delay(2000); // odottaa 2 sekuntia
 }
@@ -161,43 +200,49 @@ String read_NFC() {
 
 
 
-
+/*  convertUID() käytetään näin:
+    String NFC1 = "fa167765";
+    String converted = convertUID(NFC1); */
 // muuttaa 0-9 a-f joksikin numeroksi
-int UID_value(String x) {
-  if (x == "0") {
-    return 1;
-  } else if ( x == "1") {
-    return 2;
-  } else if ( x == "2") {
-    return 3;
-  } else if ( x == "3") {
-    return 4;
-  } else if ( x == "4") {
-    return 5;
-  } else if ( x == "5") {
-    return 6;
-  } else if ( x == "6") {
-    return 7;
-  } else if ( x == "7") {
-    return 8;
-  } else if ( x == "8") {
-    return 9;
-  } else if ( x == "9") {
-    return 0;
-  } else if ( x == "a") {
-    return -1;
-  } else if ( x == "b") {
-    return -2;
-  } else if ( x == "c") {
-    return -3;
-  } else if ( x == "d") {
-    return -4;
-  } else if ( x == "e") {
-    return -5;
-  } else if ( x == "f") {
-    return -6;
-  }
-};
+String convertUID(const String& uid) {
+    String result = "";
+
+    for (int i = 0; i < uid.length(); i++) {
+        char c = uid[i];
+        int value = 0;
+
+        if (c >= '0' && c <= '9') {
+            value = c - '0';
+        } else if (c >= 'a' && c <= 'f') {
+            value = c - 'a' + 10;
+        } else {
+            // Handle invalid characters here (if needed)
+            // For now, we'll set the value to 0 for any invalid character
+        }
+        value = constrain(value, 0, 9);
+        Serial.println(value);
+        result += String(value);
+    }
+
+    return result;
+}
+
+// päivittää NFC:n numerot arrayhin jota voi käyttää sitten statseihin. Laita sisään string UID ja array johon haluat tulokset päivittyvät
+void createStats(const String& input, int resultArray[4]) {
+    // Make sure the input length is even to group numbers in pairs
+    if (input.length() % 2 == 0) {
+        for (int i = 0; i < input.length(); i += 2) {
+            String numberPair = input.substring(i, i + 2);
+            int num1 = numberPair.substring(0, 1).toInt();
+            int num2 = numberPair.substring(1).toInt();
+            resultArray[i / 2] = num1 + num2;
+        }
+    } else {
+        // Handle the case when the input length is not even (if needed)
+        // You can set the resultArray to an error state here if required
+    }
+}
+
 
 // tallentaa esp32 muistiin muuttujan, pitäis pystyy tallentaa myös array, eli sit vaa laittaa array sisään
 void saveToMemory(String toBeSaved) {
